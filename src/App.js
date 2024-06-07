190e2a68be54f9133eecf5596815b27e29276318
Ahmad Khan <112844940+khanahmad4527@users.noreply.github.com>
2024-06-07 18:15:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, World! (Updated)</h1>
+        <h1>Ahmad Khan</h1>
       </header>
     </div>
   );
